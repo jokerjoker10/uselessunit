a2dbb54f061d8ad18097c02b79035c2320e31987
jokerjoker10 <36204071+jokerjoker10@users.noreply.github.com>
2021-12-23 17:31:55 +0100

    Enable Matomo Tracking

--- src/index.tsx
+++ src/index.tsx
@@ -4,34 +4,10 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
-// matomo tracking
-import { MatomoProvider, createInstance } from '@datapunt/matomo-tracker-react'
-var instace: any;
-if (process.env.MATOMO_URL) {
-  console.log(process.env.MATOMO_URL)
-  instace = createInstance({
-    urlBase: process.env.MATOMO_URL || '',
-    siteId: Number(process.env.MATOMO_SITE_ID) || 0,
-    disabled: process.env.MATOMO_URL ? true : false, // optional, false by default. Makes all tracking calls no-ops if set to true.
-    heartBeat: { // optional, enabled by default
-      active: true, // optional, default value: true
-      seconds: 10 // optional, default value: `15
-    },
-    configurations: { // optional, default value: {}
-      // any valid matomo configuration, all below are optional
-      disableCookies: true,
-      setSecureCookie: false,
-      setRequestMethod: 'POST'
-    }
-  })
-}
-
 ReactDOM.render(
-  <MatomoProvider value={instace}>
     <React.StrictMode>
       <App />
-    </React.StrictMode>
-  </MatomoProvider>,
+    </React.StrictMode>,
   document.getElementById('root')
 );
 
